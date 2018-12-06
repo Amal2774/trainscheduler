be95d0f242576dfc165f33d043a1ab985120427a
Ashley Alofs <asmal@Ashleys-MacBook-Pro.local>
2018-12-05 23:22:56 -0700

    added js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,17 @@
-var trainName= "";
-var destination= "";
-var firstTrainTime= "";
-var frequency= "";
-var nextArrival= "";
+let trainName= "";
+let destination= "";
+let firstTrainTime= "";
+let frequency= "";
+let nextArrival= "";
 
-var addTrainDetails= [];
+let addTrainDetails= [];
+
+function resetDetails(){
+    $("#trainNameInput").val("");
+    $("#destinationInput").val("");
+    $("#trainTimeInput").val("");
+    $("#frequencyMinInput").val("");
+};
 
 $("#submitBtn").on("click", function(event) {
     event.preventDefault();
@@ -21,7 +28,44 @@ $("#submitBtn").on("click", function(event) {
     frequency = $("#frequencyMinInput").val().trim();
     console.log(frequency);
 
+    addTrainDetails.push({
+        name: trainName,
+        destinationLocation: destination,
+        trainTime: firstTrainTime,
+        minutes: frequency,
+    
+    });
+
+    console.log(addTrainDetails);
+
+    localStorage.setItem("trainSchedule", JSON.stringify(addTrainDetails));
+
+    resetDetails();
+
 });
+
+function addTrain() {
+
+    var tBody = $("#tBody");
+    var tRow = $("<tr>");
+
+    var trainsName = $("<td>").text(trainName);
+    var destination = $("<td>").text(destinationLocation);
+    var trainFrequency = $("<td>").text(frequency);
+    var nextTrain = $("<td>").text(nextArrival);
+ 
+    tBody.append(tRow);
+    tRow.append(trainsName, destination, trainFrequency, nextTrain);
+
+    addTrain();
+};
+
+
+
+
+
+
+
 
 
 
